d4195dad51af25529856a8917871a1a98ff5bb54
carlosjoelmujicagutie <carlosjoelmujica@gmail.com>
2022-10-14 20:48:44 -0500

    primer commit de ListaApp

---
+++ src/components/ListaUsuarios/ListaUsuarios.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {users} from './../db/users'
+import { Usuario } from './Usuario'
+import './Style.css'
+
+export const ListaUsuarios = () => {
+  return (
+    <div className="Container">
+      <div className="Title"><p>Lista de Usuarios Json</p></div>
+         <ul>
+            {users.map((user) =>(
+                <Usuario key={user.id} user={user}/>
+            ))}
+         </ul>
+    </div>
+  )
+}
